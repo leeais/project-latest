888885a8d188615c16bed14f8b2cc361a51afa06
longdech <leeaiscute@gmail.com>
2025-08-31 10:53:05 +0700

    chore: initial project

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import security from 'eslint-plugin-security';
+import unicorn from 'eslint-plugin-unicorn';
+import prettier from 'eslint-plugin-prettier';
+
+export default tseslint.config([
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'jsx-a11y': jsxA11y,
+      security,
+      unicorn,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'jsx-a11y/no-autofocus': 'warn',
+      'security/detect-object-injection': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
+]);
