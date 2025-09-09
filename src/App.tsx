82517a3058cb51a374ccb4199a09a7184c638637
longnd <longnd@staka.vn>
2025-09-09 16:49:30 +0700

    style: config style to use tailwind override ant

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { StyleProvider } from '@ant-design/cssinjs';
+import { ConfigProvider } from 'antd';
+
 import AppRouter from '@/routes';
 
 export default function App() {
   return (
     <>
-      <AppRouter />
+      <StyleProvider layer>
+        <ConfigProvider>
+          <AppRouter />
+        </ConfigProvider>
+      </StyleProvider>
     </>
   );
 }
